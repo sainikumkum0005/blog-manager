17dbd6d14f7defeaa805335e52c846faaa70a864
Kumkum Saini <sainikumkum0005@gmail.com>
2026-06-12 15:24:06 +0530

    Fix Firebase author schema

--- server/models/Blog.js
+++ server/models/Blog.js
@@ -1,12 +1,26 @@
 const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema(
-  {
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
-  },
-  { timestamps: true }
+{
+title: {
+type: String,
+required: true,
+},
+
+content: {
+type: String,
+required: true,
+},
+
+author: {
+type: String,
+required: true,
+},
+},
+{
+timestamps: true,
+}
 );
 
-module.exports = mongoose.model("Blog", blogSchema); 
+module.exports = mongoose.model("Blog", blogSchema);
+ 
